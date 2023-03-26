031b1af4157d18f74f86c44dbb8960d3e759b462
JasonMendoza2008 <lhotteromain@gmail.com>
2023-03-26 03:06:53 +0200

    Test 2

--- local_playground.py
+++ local_playground.py
@@ -35,7 +35,7 @@ if __name__ == "__main__":
 
     list_min_profit: list[int] = [
         0,
-        # 1,
+        1,
         # 2,
         # 5
     ]
@@ -46,20 +46,20 @@ if __name__ == "__main__":
         # 6
     ]
     list_of_potential_ema_short_period: list[int] = [
-        5,
-        # 8,
-        # 10,
-        # 12,
-        # 15,
+        # 5,
+        8,
+        10,
+        12,
+        15,
         # 30,
     ]
     list_of_potential_ema_long_period: list[int] = [
         12,
         15,
         # 20,
-        # 30,
+        30,
         # 50,
-        # 100,
+        100,
         # 1000,
     ]
     for min_profit in list_min_profit:
